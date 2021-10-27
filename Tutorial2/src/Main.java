13d4359c9b6aab3e95147181f6a9c305c313ed2a
cyless-hj <75618206+cyless-hj@users.noreply.github.com>
2021-10-27 11:11:38 +0900

    JAVA Tutorial2

---
+++ Tutorial2/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int intType = 100;
+		double doubleType = 50.5;
+		String stringType = "정현진";
+		
+		System.out.println(intType);
+		System.out.println(doubleType);
+		System.out.println(stringType);
+
+	}
+
+}
